e9e989545be816ea3086ceacc71d1442ffbe61d6
aleksipirttimaa <aleksi.pirttimaa@oulu.fi>
2020-08-14 13:45:23 +0300

    Add missing whitespace

--- src/main.py
+++ src/main.py
@@ -39,7 +39,7 @@ def parse(package):  # Received data from sensor
         output['deveui'] = package['deviceName']
         time_sent = datetime.datetime.strptime(package["time"], "%Y-%m-%dT%H:%M:%S.%fZ")
         output['timestamp_node'] = int(time_sent.replace(tzinfo=datetime.timezone.utc).timestamp())
-        output['temperature'] = round(float(package['data'][0]['value'][1]),1)
+        output['temperature'] = round(float(package['data'][0]['value'][1]), 1)
         output['humidity'] = round(float(package['data'][0]['value'][0]))
         output['pressure'] = round(float(package['data'][1]['value'][0]))
         output['rssi'] = int(float(package['data'][2]['value'][0]))
