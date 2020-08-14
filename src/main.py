f13f27201e96e998294186f39a85d67211044c7c
Risto Korhonen <risto.korhonen@kapsi.fi>
2020-08-14 08:04:03 +0300

    Add .conf file, delete unused values and fix some smaller mistakes

--- src/main.py
+++ src/main.py
@@ -3,52 +3,52 @@ import paho.mqtt.client as mqtt
 import time
 import datetime
 import json
+import configparser
 
-MQTT_ADDR = "test.mosquitto.org"
-MQTT_PORT = 1883
-MQTT_USER = ""
-MQTT_PASSWD = ""
-MQTT_CLIENTNAME = "sensortester1234"
-MQTT_TOPIC = "test-topic"
+#Check "settings.conf.example"
+CONF_FILE = "settings.conf"
 
 app = Flask(__name__)
 data = None
 
-client = mqtt.Client(MQTT_CLIENTNAME)
+config = configparser.ConfigParser()
+config.read(CONF_FILE)
 
-@app.route('/5gtn/input', methods=['GET', 'POST'])
-def readJsonObject():           #Reads POST request, sends it to parser
-    data = request.get_json()   #and sends parsed message to MQTT-broker
+mqtt_user = config.get("MqttBroker", "user")
+mqtt_passwd = config.get("MqttBroker", "passwd")
+mqtt_host = config.get("MqttBroker", "host")
+mqtt_port = config.getint("MqttBroker", "port")
+
+client = mqtt.Client(config.get("MqttBroker", "Host"))
+
+@app.route('/input', methods=['GET', 'POST'])
+def readJsonObject():
+    data = request.get_json()
     parsed = dataParser(data)
     sendToMqtt(parsed)
     return "Data accepted"
 
 def sendToMqtt(message):
-    client.connect(MQTT_ADDR, MQTT_PORT)
-    client.loop_start()
-    client.publish(MQTT_TOPIC, json.dumps(message))
-    client.loop_stop()
-    client.disconnect
+    client.publish(config.get("MqttBroker", "out_topic"), json.dumps(message))
+ 
+def dataParser(package):	#received data from sensor
+    try:
+        output = {}
+        #pick index of value and place it in output
+        output['_msgid'] =  time.ctime().replace(" ", "-")
+        output['deveui'] = package['deviceName']
+        time_sent = datetime.datetime.strptime(package["time"], "%Y-%m-%dT%H:%M:%S.%fZ")
+        output['timestamp_node'] = int(time_sent.replace(tzinfo=datetime.timezone.utc).timestamp())
+        output['temperature'] = round(float(package['data'][0]['value'][1]),1)
+        output['humidity'] = round(float(package['data'][0]['value'][0]))
+        output['pressure'] = round(float(package['data'][1]['value'][0]))
+        output['rssi'] = int(float(package['data'][2]['value'][0]))
+        output ['battery'] = int(package["data"][3]['value'][0])/1000 #value/unit error from sensor
+        output['timestamp_parser'] = int(time.time())
+        return output
+    except (TypeError, IndexError, KeyError):
+        print("unexpected packet")
 
-def dataParser(package):				#received data from sensor
-	try:
-		output = {}
-		#pick index of value and place it in output
-		output['_msgid'] =  time.ctime().replace(" ", "-")
-		output['deveui'] = package['deviceName']
-		time_sent = datetime.datetime.strptime(package["time"], "%Y-%m-%dT%H:%M:%S.%fZ")
-		output['timestamp_node'] = int(time_sent.replace(tzinfo=datetime.timezone.utc).timestamp())
-		output['temperature'] = round(float(package['data'][0]['value'][1]))
-		output['humidity'] = round(float(package['data'][0]['value'][0]))
-		output['pressure'] = round(float(package['data'][1]['value'][0]))
-		output['rssi'] = int(float(package['data'][2]['value'][0]))
-		output['light'] = 0
-		output ['pir'] = 0
-		output ['co2'] = 0
-		output ['battery'] = int(package["data"][3]['value'][0])/1000 #value/unit error from sensor
-		output['timestamp_parser'] = int(time.time())
-		output['sound_avg'] = 0
-		output['sound_peak'] = 0
-		return output
-	except (TypeError, IndexError, KeyError):
-		print("unexpected packet")
+client.username_pw_set(mqtt_user, mqtt_passwd)
+client.connect(mqtt_host, mqtt_port)
+client.loop_start()
